76af22d0b8ff9181fba6c57e939d5a38a68b55b4
github-action-benchmark <github@users.noreply.github.com>
2025-02-28 22:01:13 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ace4825f1446f74d54805e7497895d531afb29c1

---
+++ dev/bench/data.js
@@ -0,0 +1,109 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740780073359,
+  "repoUrl": "https://github.com/mmcdermott/meds-torch-data",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mattmcdermott8@gmail.com",
+            "name": "Matthew McDermott",
+            "username": "mmcdermott"
+          },
+          "committer": {
+            "email": "mattmcdermott8@gmail.com",
+            "name": "Matthew McDermott",
+            "username": "mmcdermott"
+          },
+          "distinct": true,
+          "id": "ace4825f1446f74d54805e7497895d531afb29c1",
+          "message": "Updating benchmark workflow",
+          "timestamp": "2025-02-28T16:49:35-05:00",
+          "tree_id": "bfc26d32dfa4cbdf10cc6edbc0306c784133aae7",
+          "url": "https://github.com/mmcdermott/meds-torch-data/commit/ace4825f1446f74d54805e7497895d531afb29c1"
+        },
+        "date": 1740780072569,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Usage/Duration/Epoch",
+            "value": 0.4762592,
+            "unit": "seconds",
+            "range": 0.002732962714710154,
+            "extra": "Count: 5"
+          },
+          {
+            "name": "Usage/Memory/benchmark",
+            "value": 3144608,
+            "unit": "bytes",
+            "range": null,
+            "extra": "Count: 1"
+          },
+          {
+            "name": "Usage/Duration/read_subject_descriptors",
+            "value": 0.04604482650756836,
+            "unit": "seconds",
+            "range": null,
+            "extra": "Count: 1"
+          },
+          {
+            "name": "Usage/Duration/__getitem__",
+            "value": 0.0010603187084197998,
+            "unit": "seconds",
+            "range": 0.000043828653210516846,
+            "extra": "Count: 2000"
+          },
+          {
+            "name": "Usage/Duration/collate",
+            "value": 0.024791884422302245,
+            "unit": "seconds",
+            "range": 0.007347952888685995,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/time_delta_days",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/code",
+            "value": 28132.8,
+            "unit": "bytes",
+            "range": 8476.27859643868,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value_mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          }
+        ]
+      }
+    ]
+  }
+}
