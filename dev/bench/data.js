bfbee4cd707e583eaab935cb801fb3723de64b25
github-action-benchmark <github@users.noreply.github.com>
2025-03-18 19:23:01 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5517841900f69a9ebcbe693d5913e1afc004bfaa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742325542068,
+  "lastUpdate": 1742325781312,
   "repoUrl": "https://github.com/mmcdermott/meds-torch-data",
   "entries": {
     "Benchmark": [
@@ -3540,6 +3540,114 @@ window.BENCHMARK_DATA = {
             "value": 0.018650269508361815,
             "unit": "seconds",
             "range": 0.005235282086150517,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/time_delta_days",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/code",
+            "value": 28132.8,
+            "unit": "bytes",
+            "range": 8476.27859643868,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value_mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_code",
+            "value": 3288,
+            "unit": "bytes",
+            "range": 944.466927836956,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_numeric_value",
+            "value": 1688,
+            "unit": "bytes",
+            "range": 472.233463918478,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_numeric_value_mask",
+            "value": 488,
+            "unit": "bytes",
+            "range": 118.0583659796195,
+            "extra": "Count: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mattmcdermott8@gmail.com",
+            "name": "Matthew McDermott",
+            "username": "mmcdermott"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5517841900f69a9ebcbe693d5913e1afc004bfaa",
+          "message": "Merge pull request #8 from mmcdermott/dev\n\nFurther refinements and cleanup.",
+          "timestamp": "2025-03-18T15:20:49-04:00",
+          "tree_id": "c794e52d99a19d6fe6e3af6c3d61bd4048f698ea",
+          "url": "https://github.com/mmcdermott/meds-torch-data/commit/5517841900f69a9ebcbe693d5913e1afc004bfaa"
+        },
+        "date": 1742325780843,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Usage/Duration/Epoch",
+            "value": 0.5010000000000001,
+            "unit": "seconds",
+            "range": 0.004985219403385094,
+            "extra": "Count: 5"
+          },
+          {
+            "name": "Usage/Memory/benchmark",
+            "value": 1821079,
+            "unit": "bytes",
+            "range": null,
+            "extra": "Count: 1"
+          },
+          {
+            "name": "Usage/Duration/__getitem__",
+            "value": 0.0011391955614089965,
+            "unit": "seconds",
+            "range": 0.000080821336486098,
+            "extra": "Count: 2000"
+          },
+          {
+            "name": "Usage/Duration/collate",
+            "value": 0.020840787887573244,
+            "unit": "seconds",
+            "range": 0.005684713839071092,
             "extra": "Count: 10"
           },
           {
