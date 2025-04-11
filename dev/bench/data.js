30acc4d0a4a3aeb839c2278cf8475200ed145d5d
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 15:38:24 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for a5b67135ad985006ef2c6ca879331c7a9b5536d6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744385051899,
+  "lastUpdate": 1744385904138,
   "repoUrl": "https://github.com/mmcdermott/meds-torch-data",
   "entries": {
     "Benchmark": [
@@ -6529,6 +6529,107 @@ window.BENCHMARK_DATA = {
             "value": 0.020570850372314452,
             "unit": "seconds",
             "range": 0.008603206599082595,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/code",
+            "value": 28132.8,
+            "unit": "bytes",
+            "range": 8476.27859643868,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value_mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/time_delta_days",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_code",
+            "value": 3288,
+            "unit": "bytes",
+            "range": 944.466927836956,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_numeric_value",
+            "value": 1688,
+            "unit": "bytes",
+            "range": 472.233463918478,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_numeric_value_mask",
+            "value": 488,
+            "unit": "bytes",
+            "range": 118.0583659796195,
+            "extra": "Count: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mattmcdermott8@gmail.com",
+            "name": "Matthew McDermott",
+            "username": "mmcdermott"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a5b67135ad985006ef2c6ca879331c7a9b5536d6",
+          "message": "Merge pull request #21 from mmcdermott/no_root\n\nRemoved unused project-root file",
+          "timestamp": "2025-04-11T11:36:11-04:00",
+          "tree_id": "ce89bc17046729f234855a96728880bbd268c41f",
+          "url": "https://github.com/mmcdermott/meds-torch-data/commit/a5b67135ad985006ef2c6ca879331c7a9b5536d6"
+        },
+        "date": 1744385903246,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Usage/Duration/Epoch",
+            "value": 0.48021440000000004,
+            "unit": "seconds",
+            "range": 0.027031485120133484,
+            "extra": "Count: 5"
+          },
+          {
+            "name": "Usage/Memory/benchmark",
+            "value": 1819649,
+            "unit": "bytes",
+            "range": null,
+            "extra": "Count: 1"
+          },
+          {
+            "name": "Usage/Duration/__getitem__",
+            "value": 0.0010955853462219238,
+            "unit": "seconds",
+            "range": 0.0013171222608823487,
+            "extra": "Count: 2000"
+          },
+          {
+            "name": "Usage/Duration/collate",
+            "value": 0.019592905044555665,
+            "unit": "seconds",
+            "range": 0.0049567673962305405,
             "extra": "Count: 10"
           },
           {
