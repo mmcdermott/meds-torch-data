a79ef78a9b1c3e50f7f2e73decb7a147035b4016
github-action-benchmark <github@users.noreply.github.com>
2025-05-11 16:32:23 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 89df01daef28caecfc19db587d0187f6dc6d1f78

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746978768053,
+  "lastUpdate": 1746981143283,
   "repoUrl": "https://github.com/mmcdermott/meds-torch-data",
   "entries": {
     "Benchmark": [
@@ -10166,6 +10166,107 @@ window.BENCHMARK_DATA = {
             "value": 0.01887989044189453,
             "unit": "seconds",
             "range": 0.005352289270134068,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/code",
+            "value": 28132.8,
+            "unit": "bytes",
+            "range": 8476.27859643868,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/numeric_value_mask",
+            "value": 3593.6,
+            "unit": "bytes",
+            "range": 1059.534824554835,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/time_delta_days",
+            "value": 14110.4,
+            "unit": "bytes",
+            "range": 4238.13929821934,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_code",
+            "value": 3288,
+            "unit": "bytes",
+            "range": 944.466927836956,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_numeric_value",
+            "value": 1688,
+            "unit": "bytes",
+            "range": 472.233463918478,
+            "extra": "Count: 10"
+          },
+          {
+            "name": "Outputs/BatchSize/static_numeric_value_mask",
+            "value": 488,
+            "unit": "bytes",
+            "range": 118.0583659796195,
+            "extra": "Count: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mattmcdermott8@gmail.com",
+            "name": "Matthew McDermott",
+            "username": "mmcdermott"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "89df01daef28caecfc19db587d0187f6dc6d1f78",
+          "message": "Merge pull request #44 from mmcdermott/dev\n\nNew Release Candidate: Adds capabilities to prepend static data, use an index dataframe without labels, and pin memory.",
+          "timestamp": "2025-05-12T00:30:03+08:00",
+          "tree_id": "528a5d6dc4bee04d5d8fc8184966f61abf69a51a",
+          "url": "https://github.com/mmcdermott/meds-torch-data/commit/89df01daef28caecfc19db587d0187f6dc6d1f78"
+        },
+        "date": 1746981142295,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Usage/Duration/Epoch",
+            "value": 0.4435504,
+            "unit": "seconds",
+            "range": 0.0035901823491253137,
+            "extra": "Count: 5"
+          },
+          {
+            "name": "Usage/Memory/benchmark",
+            "value": 2871124,
+            "unit": "bytes",
+            "range": null,
+            "extra": "Count: 1"
+          },
+          {
+            "name": "Usage/Duration/__getitem__",
+            "value": 0.0010110783576965333,
+            "unit": "seconds",
+            "range": 0.00004814455036770619,
+            "extra": "Count: 2000"
+          },
+          {
+            "name": "Usage/Duration/collate",
+            "value": 0.01840038299560547,
+            "unit": "seconds",
+            "range": 0.005152010741959082,
             "extra": "Count: 10"
           },
           {
